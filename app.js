c7237cd84f908b79e49a9413a69fa58522c93221
Asya Lem <anundam@gmail.com>
2017-08-01 16:22:00 +0300

    formidable changed to multer

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
 const
-    express = require("express");
-    app = express();
-    path = require('path');
-    formidable = require('formidable');
-    fs = require('fs');
+     express = require('express'),
+     multer = require('multer'),
+     app = express();
 
-app.use(express.static(__dirname + '/public'));
-
-app.get("/" ,(request, response) => {
-    response.send("<h2>oh, hi mark</h2>");
-});
-app.get('/download', (req, res) =>{
-    var file = __dirname + '/downloads/eva.png';
+app.get('/download', (req, res, next) =>{
+    let file = __dirname + '/downloads/eva.png';
     console.log(file);
     res.download(file);
+    });
+    let storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, './upload');
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);
+    }
+    });
+    let upload = multer({ storage : storage }).array('userPhoto',10);
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/upload',(req, res) => {
-
-    var form = new formidable.IncomingForm();
-
-    form.multiples = true;
-
-    form.uploadDir = path.join(__dirname, '/upload');
-
-    form.on('file',(field, file) => {
-        fs.rename(file.path, path.join(form.uploadDir, file.name));
+app.post('/upload',(req,res, next) =>{
+    upload(req,res, (err) => {
+        console.log(req.body);
+        console.log(req.files);
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
     });
-
-    form.on('error', (err) => {
-        console.log('An error has occured: \n' + err);
-    });
-
-    form.on('end', () => {
-        res.end('success');
-    });
-
-    form.parse(req);
-
 });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+    console.log("Working on port 3000");
 });
-
